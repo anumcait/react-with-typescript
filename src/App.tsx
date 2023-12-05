a2fce8c2ce122a98fa629ffbd5188d208791733b
Anu <anumcait@gmail.com>
2023-12-05 16:55:42 +0530

    After changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List'
+import AddToList from './components/AddToList';
+
+
+
+export interface IState{
+  people:{
+    name:string,
+    age:number,
+    url:string,
+    note?:string
+
+  }[]
+}
 
 function App() {
+  const [people,setPeople]=useState<IState["people"]>([
+    {
+      name:"Aliveni",
+      age:36,
+      url:"https://png.pngtree.com/element_our/20190528/ourmid/pngtree-small-url-icon-opened-on-the-computer-image_1132275.jpg",
+      note:"this is my first image"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Data </h1>
+      <List  people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
+     
     </div>
   );
 }
